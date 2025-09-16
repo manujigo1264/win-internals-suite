e77f7945438c6fa96e1c1e385c0cd86592e83342
manujigo1264 <emmanuel.jigo29@gmail.com>
2025-09-16 17:15:03 -0400

    Initial commit with solution and winsuite.cpp

---
+++ win-internals-suite/win-internals-suite/winsuite.cpp
@@ -0,0 +1,14 @@
+// winsuite.cpp - Windows Internals Suite (single-file, no external deps)
+// Build: x64, C++17+, Link with Psapi.lib; Bcrypt.lib
+
+#define NOMINMAX
+#include <Windows.h>
+#include <tlhelp32.h> // process/module snapshots
+#include <psapi.h> // process/module info utils
+#include <bcrypt.h> // SHA-256 hashing
+#pragma comment(lib, "Psapi.lib") // ensures linker pulls Psapi.lib
+#pragma comment(lib, "Bcrypt.lib") // ensures linker pulls Bcrypt.lib
+
+int main() {
+	return 0;
+}
